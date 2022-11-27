eb656d2ddef7dc63a53b9fedfa091f95d8986f3e
hido02 <bestdohee@gmail.com>
2022-11-27 18:32:02 +0900

    fetch: db 연동 시도2

--- src/handler/action/ExampleAction.java
+++ src/handler/action/ExampleAction.java
@@ -1,17 +1,17 @@
-//package handler.action;
-//
-//import com.google.gson.Gson;
-//import handler.dao.ExampleDAO;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.swing.*;
-//
-//public class ExampleAction implements Action {
-//    @Override
-//    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        Gson gson = new Gson();
-//        request.setAttribute("ExampleData", gson.toJson(ExampleDAO.getInstance().getAllExampleData()));
-//        return "RequestDispatcher:jsp/example.jsp";
-//    }
-//}
+package handler.action;
+
+import com.google.gson.Gson;
+import common.controller.Action;
+import handler.dao.ExampleDAO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExampleAction implements Action {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Gson gson = new Gson();
+        request.setAttribute("ExampleData", gson.toJson(ExampleDAO.getInstance().getAllExampleData()));
+        return "RequestDispatcher:jsp/example.jsp";
+    }
+}
